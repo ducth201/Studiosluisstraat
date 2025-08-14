5977210e12e308b8294a64be178b877140cd0a91
ducth201 <b.engelsman@gmail.com>
2025-08-14 17:36:00 +0200

    added footers and top header
    added footers and top header

--- wp-content/themes/sss/build/myfooter/render.php
+++ wp-content/themes/sss/build/myfooter/render.php
@@ -4,5 +4,17 @@
  */
 ?>
 <footer>
-	<h4>Footer</h4>
+	<div>
+		<span>Studio Sluisstraat</span>
+		<span>Sluisstraat 34 hs</span>
+		<span>1075TG Amsterdam</span>		
+		<span>E-mail: <a href="mailto:info@studiosluisstraat.nl">info@studiosluisstraat.nl</a></span>
+		<span>Telefoon: <a href="tel:+31642070591">+31642070591</a></span>
+	</div>
+	<div>
+		<span>KvK: 55873898</span>
+		<span>BTW: NL 188408174B02</span>
+		<span>&nbsp;</span>
+		<span><a href="https://nl.linkedin.com/in/bator">Linkedin</a></span>
+	</div>
 </footer>
